4155fddec4b873e38ed5c3ac0035cdbea5431a64
Breno Assis <breno.assis9@outlook.com.br>
2024-12-06 07:39:53 -0300

    implementado conexão com banco via entity framework core.

--- code/expense/src/expense.Api/Program.cs
+++ code/expense/src/expense.Api/Program.cs
@@ -1,4 +1,9 @@
+using expense.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ code/expense/src/expense.Infrastructure.Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace expense.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+    { }
+}
