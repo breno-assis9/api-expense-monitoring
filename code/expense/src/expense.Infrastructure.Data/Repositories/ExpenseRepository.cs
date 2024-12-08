0251a83880ba16836797850c95286fe321a370fe
Breno Assis <breno.assis9@outlook.com.br>
2024-12-08 07:04:06 -0300

    implementação do padrao repository para despesa.

---
+++ code/expense/src/expense.Infrastructure.Data/Repositories/ExpenseRepository.cs
@@ -0,0 +1,49 @@
+﻿using expense.Domain.Entities;
+using expense.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expense.Infrastructure.Data.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(ExpenseEntity expense)
+        {
+            await _context.Set<ExpenseEntity>().AddAsync(expense);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(ExpenseEntity expense)
+        {
+             _context.Set<ExpenseEntity>().Remove(expense);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ExpenseEntity>> GetAll()
+        {
+            return await _context.Set<ExpenseEntity>().ToListAsync();
+        }
+
+        public async Task<ExpenseEntity> GetById(Guid id)
+        {
+            return await _context.Set<ExpenseEntity>().FindAsync(id);
+        }
+
+        public async Task Update(ExpenseEntity expense)
+        {
+            _context.Set<ExpenseEntity>().Update(expense);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
